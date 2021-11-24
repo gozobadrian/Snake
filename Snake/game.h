2032a1c72f38c50e50f4af78b57a221627a058f8
Adrian Mihai GOZOB <gozobadrian@yahoo.ro>
2021-11-24 20:26:37 +0200

    Code hygene; updated gitignore.

--- Snake/game.h
+++ Snake/game.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
+
 #define upArrowKey 72
 #define downArrowKey 80
 #define leftArrowKey 75
@@ -16,59 +17,89 @@ class Map
 {
     public:
     char map[101][101];
-    void generateMap()///Initialise map
+    void generateMap() // Initialise map
     {
-        for(int i=1;i<=mapX;i++)
-            for(int j=1;j<=mapY;j++)
-                if(i==1||i==mapX) map[i][j]='-';
-                else if(j==1||j==mapY) map[i][j]='|';
-                else map[i][j]='_';
-    }
-    void drawMap()///Prints the map
-    {
-        for(int i=1;i<=mapX;i++)
+        for (int i = 1; i <= mapX; i++)
         {
-            for(int j=1;j<=mapY;j++)
-                if(map[i][j]=='_') cout<<" ";
-                else cout<<map[i][j];
-            cout<<'\n';
+            for (int j = 1; j <= mapY; j++)
+            {
+                if (i == 1 || i == mapX)
+                {
+                    map[i][j] = '-';
+                }
+                else if (j == 1 || j == mapY)
+                {
+                    map[i][j] = '|';
+                }
+                else
+                {
+                    map[i][j] = '_';
+                }
+            }
         }
     }
-    void addPlayerToMap(int x,int y)///Adds an 'O' to the map on (x,y) position, i.e. a part of the snake's "body"
+
+    void drawMap() // Prints the map
     {
-        map[x][y]='O';
+        for (int i = 1; i <= mapX; i++)
+        {
+            for (int j = 1; j <= mapY; j++)
+            {
+                if (map[i][j] == '_')
+                {
+                    cout << " ";
+                }
+                else
+                {
+                    cout << map[i][j];
+                }
+            }
+            cout << '\n';
+        }
     }
-    void addPlayerHeadToMap(int x,int y)///Adds a '0' to the map on (x,y) position, i.e. the snake's "head"
+
+    void addPlayerToMap(int x, int y) // Adds an 'O' to the map on (x,y) position, i.e. a part of the snake's "body"
     {
-        map[x][y]='0';
+        map[x][y] = 'O';
     }
-    void clearPlayerPosition(int x,int y)///Clears position (x,y) ('_' is considered an empty space on the map)
+
+    void addPlayerHeadToMap(int x, int y) // Adds a '0' to the map on (x,y) position, i.e. the snake's "head"
     {
-        map[x][y]='_';
+        map[x][y] = '0';
     }
-    bool isMovePossible(int x,int y)///Checks if moving to (x,y) is possible (i.e. when the "head" moves to an empty space - '_' - or a coin - the heart symbol)
+
+    void clearPlayerPosition(int x, int y) // Clears position (x,y) ('_' is considered an empty space on the map)
     {
-        if(map[x][y]=='_'||map[x][y]==3) return true;
+        map[x][y] = '_';
+    }
+
+    bool isMovePossible(int x, int y) // Checks if moving to (x,y) is possible (i.e. when the "head" moves to an empty space - '_' - or a coin - the heart symbol)
+    {
+        if (map[x][y] == '_' || map[x][y] == 3) return true;
         return false;
     }
-    bool isNextMoveACoin(int x,int y)///Checks if you are about to get a coin if you move to position (x,y)
+
+    bool isNextMoveACoin(int x, int y) // Checks if you are about to get a coin if you move to position (x,y)
     {
-        if(map[x][y]==3) return true;
+        if (map[x][y] == 3) return true;
         return false;
     }
-    bool checkAvailability(int x,int y)///Checks if the (x,y) position is empty ('_') - this is used to add coins to the map
+
+    bool checkAvailability(int x, int y) // Checks if the (x,y) position is empty ('_') - this is used to add coins to the map
     {
-        if(map[x][y]=='_') return true;
+        if (map[x][y] == '_') return true;
         return false;
     }
-    void addCoin(int x,int y)///Adds a coin to the map on position (x,y)
+
+    void addCoin(int x, int y) // Adds a coin to the map on position (x,y)
     {
-        map[x][y]=3;
+        map[x][y] = 3;
     }
-    Map()///Constructor initialising the map width and height
+
+    Map() // Constructor initialising the map width and height
     {
-        mapX=20;
-        mapY=50;
+        mapX = 20;
+        mapY = 50;
     }
 };
 
@@ -77,7 +108,7 @@ class Player
     public:
     struct coords
     {
-        int playerX,playerY;
+        int playerX, playerY;
     };
     coords chain[300];
     bool isPlayerAlive;
@@ -85,52 +116,58 @@ class Player
     int lenght;
     int pauseTime;
     string name;
-    void dies()///"Kills" player
+
+    void dies() // "Kills" player
     {
-        isPlayerAlive=false;
+        isPlayerAlive = false;
     }
-    void quits()///Player quits (if Esc key is pressed)
+
+    void quits() // Player quits (if Esc key is pressed)
     {
-        hasPlayerQuit=true;
+        hasPlayerQuit = true;
     }
-    void modifyCoords()///Moves every snake "chain" forward
+
+    void modifyCoords() // Moves every snake "chain" forward
     {
-        for(int i=lenght;i>=2;i--)
+        for (int i = lenght; i >= 2; i--)
         {
-            chain[i].playerX=chain[i-1].playerX;
-            chain[i].playerY=chain[i-1].playerY;
+            chain[i].playerX = chain[i - 1].playerX;
+            chain[i].playerY = chain[i - 1].playerY;
         }
     }
-    void addChain()///Adds one more chain to the snake
+
+    void addChain() // Adds one more chain to the snake
     {
         lenght++;
     }
-    void resetPlayer()///Reinitialises the snake lenght, coordinates and other stats
+
+    void resetPlayer() // Reinitialises the snake lenght, coordinates and other stats
     {
-        lenght=4;
-        int startX=mapX/2;
-        int startY=mapY/2+lenght/2;
-        for(int i=1;i<=lenght;i++)
+        lenght = 4;
+        int startX = mapX / 2;
+        int startY = mapY / 2 + lenght / 2;
+        for (int i = 1; i <= lenght; i++)
         {
-            chain[i].playerX=startX;
-            chain[i].playerY=startY-i+1;
+            chain[i].playerX = startX;
+            chain[i].playerY = startY - i + 1;
         }
-        isPlayerAlive=true;
-        hasPlayerQuit=false;
-        pauseTime=50;
+        isPlayerAlive = true;
+        hasPlayerQuit = false;
+        pauseTime = 50;
     }
-    Player()///Constructor that initialises snake stats
+
+    Player() // Constructor that initialises snake stats
     {
-        lenght=4;
-        int startX=mapX/2;
-        int startY=mapY/2+lenght/2;
-        for(int i=1;i<=lenght;i++)
+        lenght = 4;
+        int startX = mapX / 2;
+        int startY = mapY / 2 + lenght / 2;
+        for (int i = 1; i <= lenght; i++)
         {
-            chain[i].playerX=startX;
-            chain[i].playerY=startY-i+1;
+            chain[i].playerX = startX;
+            chain[i].playerY = startY - i + 1;
         }
-        isPlayerAlive=true;
-        hasPlayerQuit=false;
-        pauseTime=50;
+        isPlayerAlive = true;
+        hasPlayerQuit = false;
+        pauseTime = 50;
     }
 };
--- Snake/main.cpp
+++ Snake/main.cpp
@@ -19,16 +19,18 @@ bool dontPauseThisTime=false;
 
 void printMenu();
 
-string encryptDecrypt(string toEncrypt)///Encrypts/Decrypts highscore file
+string encodeDecode(string toEncrypt) // Encrypts/Decrypts highscore file
 {
-    char key[5]={'S','N','A','K','E'};
+    char key[5] = { 'S', 'N', 'A', 'K', 'E' };
     string output = toEncrypt;
-    for (int i=0;i<toEncrypt.size();i++)
-        output[i]=toEncrypt[i]^key[i%(sizeof(key)/sizeof(char))];
+    for (int i = 0; i < toEncrypt.size(); i++)
+    {
+        output[i] = toEncrypt[i] ^ key[i % (sizeof(key) / sizeof(char))];
+    }
     return output;
 }
 
-void clearscreen()///Sets the console cursor position to (0,0)
+void clearscreen() // Sets the console cursor position to (0,0)
 {
     HANDLE hOut;
     COORD Position;
@@ -40,46 +42,60 @@ void clearscreen()///Sets the console cursor position to (0,0)
     SetConsoleCursorPosition(hOut, Position);
 }
 
-void addPlayer()///Ads player to map
+void addPlayer() // Adds player to map
 {
-    for(int i=1;i<=player1.lenght;i++)
-        if(i==1) map1.addPlayerHeadToMap(player1.chain[i].playerX,player1.chain[i].playerY);
-        else map1.addPlayerToMap(player1.chain[i].playerX,player1.chain[i].playerY);
+    for (int i = 1; i <= player1.lenght; i++)
+    {
+        if (i == 1)
+        {
+            map1.addPlayerHeadToMap(player1.chain[i].playerX, player1.chain[i].playerY);
+        }
+        else map1.addPlayerToMap(player1.chain[i].playerX, player1.chain[i].playerY);
+    }
 }
 
-void loadHighScore()///Reads highscore file
+void loadHighScore() // Reads highscore file
 {
     string encryptedString;
     int encryptedLenght;
     ifstream scoreIn("high_score.snk");
-    scoreIn>>encryptedString;
-    scoreIn>>encryptedLenght;
-    player1.name=encryptDecrypt(encryptedString);
-    highscore=(encryptedLenght-313)/187;
+    scoreIn >> encryptedString;
+    scoreIn >> encryptedLenght;
+    player1.name = encodeDecode(encryptedString);
+    highscore = (encryptedLenght - 313) / 187;
     scoreIn.close();
 }
 
-bool itIsNotAvailable()///Checks if (X,Y) position is empty
+bool itIsNotAvailable() // Checks if (X,Y) position is empty
 {
-    if(map1.checkAvailability(X,Y)) return false;
+    if (map1.checkAvailability(X, Y))
+    {
+        return false;
+    }
     return true;
 }
 
-void generateCoinCoords()///Ads coin to a random generated empty position
+void generateCoinCoords() // Ads coin to a random generated empty position
 {
     do
     {
         srand(time(NULL));
-        int rdm=rand()%mapY+1;
-        if(rdm==1) rdm++;
-        else if(rdm==mapX) rdm--;
-        X=rdm%mapX;
-        Y=rdm;
-    }while(itIsNotAvailable());
-    map1.addCoin(X,Y);
+        int rdm = rand() % mapY + 1;
+        if (rdm == 1)
+        {
+            rdm++;
+        }
+        else if (rdm == mapX)
+        {
+            rdm--;
+        }
+        X = rdm % mapX;
+        Y = rdm;
+    } while (itIsNotAvailable());
+    map1.addCoin(X, Y);
 }
 
-void initialLoad()///Loads highscore from file, generates map, adds snake to map and generates first coin
+void initialLoad() // Loads highscore from file, generates map, adds snake to map and generates first coin
 {
     loadHighScore();
     map1.generateMap();
@@ -87,19 +103,21 @@ void initialLoad()///Loads highscore from file, generates map, adds snake to map
     generateCoinCoords();
 }
 
-void movePlayer(int x,int y)///Moves snake "head"
+void movePlayer(int x, int y) // Moves snake "head"
 {
-    if(!gotCoin)
-        map1.clearPlayerPosition(player1.chain[player1.lenght].playerX,player1.chain[player1.lenght].playerY);
-    map1.addPlayerToMap(player1.chain[1].playerX,player1.chain[1].playerY);
+    if (!gotCoin)
+    {
+        map1.clearPlayerPosition(player1.chain[player1.lenght].playerX, player1.chain[player1.lenght].playerY);
+    }
+    map1.addPlayerToMap(player1.chain[1].playerX, player1.chain[1].playerY);
     player1.modifyCoords();
-    map1.addPlayerHeadToMap(x,y);
-    player1.chain[1].playerX=x;
-    player1.chain[1].playerY=y;
-    gotCoin=false;
+    map1.addPlayerHeadToMap(x, y);
+    player1.chain[1].playerX = x;
+    player1.chain[1].playerY = y;
+    gotCoin = false;
 }
 
-void moveToDirection()///Moves the snake to the current direction
+void moveToDirection() // Moves the snake to the current direction
 {
     switch(direction)
     {
@@ -112,10 +130,14 @@ void moveToDirection()///Moves the snake to the current direction
                 dontPauseThisTime=true;
                 cmd_change_color();
             }
-            if(!map1.isMovePossible(player1.chain[1].playerX-1,player1.chain[1].playerY))
+            if (!map1.isMovePossible(player1.chain[1].playerX - 1, player1.chain[1].playerY))
+            {
                 player1.dies();
+            }
             else
-                movePlayer(player1.chain[1].playerX-1,player1.chain[1].playerY);
+            {
+                movePlayer(player1.chain[1].playerX - 1, player1.chain[1].playerY);
+            }
             break;
         case downArrowKey:
             if(map1.isNextMoveACoin(player1.chain[1].playerX+1,player1.chain[1].playerY))
@@ -126,10 +148,14 @@ void moveToDirection()///Moves the snake to the current direction
                 dontPauseThisTime=true;
                 cmd_change_color();
             }
-            if(!map1.isMovePossible(player1.chain[1].playerX+1,player1.chain[1].playerY))
+            if (!map1.isMovePossible(player1.chain[1].playerX + 1, player1.chain[1].playerY))
+            {
                 player1.dies();
+            }
             else
-                movePlayer(player1.chain[1].playerX+1,player1.chain[1].playerY);
+            {
+                movePlayer(player1.chain[1].playerX + 1, player1.chain[1].playerY);
+            }
             break;
         case leftArrowKey:
             if(map1.isNextMoveACoin(player1.chain[1].playerX,player1.chain[1].playerY-1))
@@ -140,10 +166,14 @@ void moveToDirection()///Moves the snake to the current direction
                 dontPauseThisTime=true;
                 cmd_change_color();
             }
-            if(!map1.isMovePossible(player1.chain[1].playerX,player1.chain[1].playerY-1))
+            if (!map1.isMovePossible(player1.chain[1].playerX, player1.chain[1].playerY - 1))
+            {
                 player1.dies();
+            }
             else
-                movePlayer(player1.chain[1].playerX,player1.chain[1].playerY-1);
+            {
+                movePlayer(player1.chain[1].playerX, player1.chain[1].playerY - 1);
+            }
             break;
         case rightArrowKey:
             if(map1.isNextMoveACoin(player1.chain[1].playerX,player1.chain[1].playerY+1))
@@ -154,10 +184,14 @@ void moveToDirection()///Moves the snake to the current direction
                 dontPauseThisTime=true;
                 cmd_change_color();
             }
-            if(!map1.isMovePossible(player1.chain[1].playerX,player1.chain[1].playerY+1))
+            if (!map1.isMovePossible(player1.chain[1].playerX, player1.chain[1].playerY + 1))
+            {
                 player1.dies();
+            }
             else
-                movePlayer(player1.chain[1].playerX,player1.chain[1].playerY+1);
+            {
+                movePlayer(player1.chain[1].playerX, player1.chain[1].playerY + 1);
+            }
             break;
         case escapeKey:
             player1.quits();
@@ -165,71 +199,89 @@ void moveToDirection()///Moves the snake to the current direction
     }
 }
 
-void printScore()///Prints the highscore and current score
+void printScore() // Prints the highscore and current score
 {
     clearscreen();
-    cout<<"HIGHSCORE="<<highscore<<" (by "<<player1.name<<")"<<'\n';
-    cout<<"LENGHT="<<player1.lenght<<'\n';
+    cout << "HIGHSCORE=" << highscore << " (by " << player1.name << ")" << '\n';
+    cout << "LENGHT=" << player1.lenght << '\n';
 }
 
-void printEndMessage()///Prints a message when you die or quit
+void printEndMessage() // Prints a message when you die or quit
 {
-    if(!player1.hasPlayerQuit)
-        cout<<"YOU LOST! PRESS ANY KEY TO CONTINUE...\n",Sleep(1000),getch();
-    else
-        cout<<"YOU QUIT. PRESS ANY KEY TO CONTINUE...\n",Sleep(1000),getch();
-}
-
-void initialPrint()///Initially prints the map and waits until the player pressed an arrow key or escape
-{
-    printScore();
-    map1.drawMap();
-    while(key!=rightArrowKey&&key!=upArrowKey&&key!=downArrowKey&&key!=escapeKey)
-        key=getch();
-    if(key==escapeKey)
-        player1.quits();
-    else
-        direction=key;
-}
-
-void checkIfDirectionChanged()///Checks if player pressed an arrow key and changes the direction the snakes moves to
-{
-    if(kbhit())
+    if (!player1.hasPlayerQuit)
     {
-        key=getch();
-        dontPauseThisTime=true;
-        if(!(key==upArrowKey&&direction==downArrowKey)&&!(key==downArrowKey&&direction==upArrowKey)&&!(key==leftArrowKey&&direction==rightArrowKey)&&!(key==rightArrowKey&&direction==leftArrowKey))
-            direction=key;
+        cout << "YOU LOST! PRESS ANY KEY TO CONTINUE...\n", Sleep(1000), getch();
+    }
+    else
+    {
+        cout << "YOU QUIT. PRESS ANY KEY TO CONTINUE...\n", Sleep(1000), getch();
     }
 }
 
-void newHighscore()///Prints new highscore message and
+void initialPrint() // Initially prints the map and waits until the player pressed an arrow key or escape
 {
-    cout<<"CONGRATULATIONS! YOU SET A NEW HIGHSCORE!\n";
-    cout<<"PLEASE INPUT YOUR NAME...\n";
-    cin>>player1.name;
-    ofstream scoreOut("high_score.snk");
-    if(player1.name[0]==NULL)
-        scoreOut<<"*NO NAME*"<<'\n';
+    printScore();
+    map1.drawMap();
+    while (key != rightArrowKey && key != upArrowKey && key != downArrowKey && key != escapeKey)
+    {
+        key = getch();
+    }
+    if (key == escapeKey)
+    {
+        player1.quits();
+    }
     else
-        scoreOut<<encryptDecrypt(player1.name)<<'\n';
-    int encryptedLenght=(player1.lenght*187)+313;
-    scoreOut<<encryptedLenght;
+    {
+        direction = key;
+    }
+}
+
+void checkIfDirectionChanged() // Checks if player pressed an arrow key and changes the direction the snakes moves to
+{
+    if (kbhit())
+    {
+        key = getch();
+        dontPauseThisTime = true;
+        if (!(key == upArrowKey && direction == downArrowKey) && !(key == downArrowKey && direction == upArrowKey) && !(key == leftArrowKey && direction == rightArrowKey) && !(key == rightArrowKey && direction == leftArrowKey))
+        {
+            direction = key;
+        }
+    }
+}
+
+void newHighscore() // Prints new highscore message and
+{
+    cout << "CONGRATULATIONS! YOU SET A NEW HIGHSCORE!\n";
+    cout << "PLEASE INPUT YOUR NAME...\n";
+    cin >> player1.name;
+    ofstream scoreOut("high_score.snk");
+    if (player1.name[0] == NULL)
+    {
+        scoreOut << "*NO NAME*" << '\n';
+    }
+    else
+    {
+        scoreOut << encodeDecode(player1.name) << '\n';
+    }
+    int encryptedLenght = (player1.lenght * 187) + 313;
+    scoreOut << encryptedLenght;
     scoreOut.close();
 }
 
-void pause(int pauseTime)///Checks if the game has to pause
+void pause(int pauseTime) // Checks if the game has to pause
 {
-    if(!dontPauseThisTime)
-            Sleep(pauseTime);
-        dontPauseThisTime=false;
+    if (!dontPauseThisTime)
+    {
+        Sleep(pauseTime);
+    }
+    dontPauseThisTime = false;
 }
 
-void startGame()///Starts the game
+void startGame() // Starts the game
 {
     system("cls");
     initialPrint();
-    while(player1.isPlayerAlive&&!player1.hasPlayerQuit)
+    while (player1.isPlayerAlive && !player1.hasPlayerQuit)
     {
         printScore();
         map1.drawMap();
@@ -237,37 +289,41 @@ void startGame()///Starts the game
         moveToDirection();
         pause(player1.pauseTime);
     }
-    if(player1.lenght>highscore)
+    if (player1.lenght > highscore)
+    {
         newHighscore();
+    }
     else
+    {
         printEndMessage();
+    }
     printMenu();
 }
 
-void helpMenu()///Prints the help menu
+void helpMenu() // Prints the help menu
 {
     system("cls");
-    cout<<"     .---- |   | .---. |   / .----"<<'\n';
-    cout<<"     |     |   | |   | |  /  |    "<<'\n';
-    cout<<"     |     |\\  | |   | | /   |    "<<'\n';
-    cout<<"     ----- | \\ | |---| |<    |----"<<'\n';
-    cout<<"         | |  \\| |   | | \\   |    "<<'\n';
-    cout<<"         | |   | |   | |  \\  |    "<<'\n';
-    cout<<"     ----. |   | |   | |   \\ .----"<<'\n';
-    cout<<"                        by Adrian Gozob"<<"\n\n\n";
-    cout<<"This is a remake of the Snake video game,"<<'\n';
-    cout<<"  coded in C++ using standard libraries."<<"\n\n";
-    cout<<"             INSTRUCTIONS              "<<'\n';
-    cout<<"---------------------------------------"<<'\n';
-    cout<<"   Your goal is to eat hearths("<<(char)3<<") and"<<'\n';
-    cout<<"   get longer without biting yourself  "<<'\n';
-    cout<<"     or hitting the walls (| and -).   "<<"\n\n";
-    cout<<"                CONTROLS               "<<'\n';
-    cout<<"---------------------------------------"<<'\n';
-    cout<<"    Use the arrow keys to change the   "<<'\n';
-    cout<<"movement direction of the snake. Press "<<'\n';
-    cout<<"\"Escape\" to quit the game or any other"<<'\n';
-    cout<<"        key to pause the game.          "<<'\n';
+    cout << "     .---- |   | .---. |   / .----" << '\n';
+    cout << "     |     |   | |   | |  /  |    " << '\n';
+    cout << "     |     |\\  | |   | | /   |    " << '\n';
+    cout << "     ----- | \\ | |---| |<    |----" << '\n';
+    cout << "         | |  \\| |   | | \\   |    " << '\n';
+    cout << "         | |   | |   | |  \\  |    " << '\n';
+    cout << "     ----. |   | |   | |   \\ .----" << '\n';
+    cout << "                        by Adrian Gozob" << "\n\n\n";
+    cout << "This is a remake of the Snake video game," << '\n';
+    cout << "  coded in C++ using standard libraries." << "\n\n";
+    cout << "             INSTRUCTIONS              " << '\n';
+    cout << "---------------------------------------" << '\n';
+    cout << "   Your goal is to eat hearths(" << (char)3 << ") and" << '\n';
+    cout << "   get longer without biting yourself  " << '\n';
+    cout << "     or hitting the walls (| and -).   " << "\n\n";
+    cout << "                CONTROLS               " << '\n';
+    cout << "---------------------------------------" << '\n';
+    cout << "    Use the arrow keys to change the   " << '\n';
+    cout << "movement direction of the snake. Press " << '\n';
+    cout << "\"Escape\" to quit the game or any other" << '\n';
+    cout << "        key to pause the game.          " << '\n';
     getch();
     printMenu();
 }
@@ -275,67 +331,82 @@ void helpMenu()///Prints the help menu
 void printMenu()
 {
     system("color 0a");
-    int option=1;
+    int option = 1;
     do
     {
-        key=0;
+        key = 0;
         system("cls");
-        cout<<"     .---- |   | .---. |   / .----"<<'\n';
-        cout<<"     |     |   | |   | |  /  |    "<<'\n';
-        cout<<"     |     |\\  | |   | | /   |    "<<'\n';
-        cout<<"     ----- | \\ | |---| |<    |----"<<'\n';
-        cout<<"         | |  \\| |   | | \\   |    "<<'\n';
-        cout<<"         | |   | |   | |  \\  |    "<<'\n';
-        cout<<"     ----. |   | |   | |   \\ .----"<<'\n';
-        cout<<"                        by Adrian Gozob"<<"\n\n\n";
-        if(option==1)
+        cout << "     .---- |   | .---. |   / .----" << '\n';
+        cout << "     |     |   | |   | |  /  |    " << '\n';
+        cout << "     |     |\\  | |   | | /   |    " << '\n';
+        cout << "     ----- | \\ | |---| |<    |----" << '\n';
+        cout << "         | |  \\| |   | | \\   |    " << '\n';
+        cout << "         | |   | |   | |  \\  |    " << '\n';
+        cout << "     ----. |   | |   | |   \\ .----" << '\n';
+        cout << "                        by Adrian Gozob" << "\n\n\n";
+        if (option == 1)
         {
-            cout<<"                ||PLAY||               "<<"\n\n";
-            cout<<"               DIFFICULTY              "<<"\n\n";
-            cout<<"                  HELP                 "<<"\n\n";
-            cout<<"                  QUIT                 "<<"\n\n";
+            cout << "                ||PLAY||               " << "\n\n";
+            cout << "               DIFFICULTY              " << "\n\n";
+            cout << "                  HELP                 " << "\n\n";
+            cout << "                  QUIT                 " << "\n\n";
         }
-        else if(option==2)
+        else if (option == 2)
         {
-            cout<<"                  PLAY                 "<<"\n\n";
-            cout<<"             ||DIFFICULTY||            "<<"\n\n";
-            cout<<"                  HELP                 "<<"\n\n";
-            cout<<"                  QUIT                 "<<"\n\n";
+            cout << "                  PLAY                 " << "\n\n";
+            cout << "             ||DIFFICULTY||            " << "\n\n";
+            cout << "                  HELP                 " << "\n\n";
+            cout << "                  QUIT                 " << "\n\n";
         }
-        else if(option==3)
+        else if (option == 3)
         {
-            cout<<"                  PLAY                 "<<"\n\n";
-            cout<<"               DIFFICULTY              "<<"\n\n";
-            cout<<"                ||HELP||               "<<"\n\n";
-            cout<<"                  QUIT                 "<<"\n\n";
+            cout << "                  PLAY                 " << "\n\n";
+            cout << "               DIFFICULTY              " << "\n\n";
+            cout << "                ||HELP||               " << "\n\n";
+            cout << "                  QUIT                 " << "\n\n";
         }
-        else if(option==4)
+        else if (option == 4)
         {
-            cout<<"                  PLAY                 "<<"\n\n";
-            cout<<"               DIFFICULTY              "<<"\n\n";
-            cout<<"                  HELP                 "<<"\n\n";
-            cout<<"                ||QUIT||               "<<"\n\n";
+            cout << "                  PLAY                 " << "\n\n";
+            cout << "               DIFFICULTY              " << "\n\n";
+            cout << "                  HELP                 " << "\n\n";
+            cout << "                ||QUIT||               " << "\n\n";
         }
-        while(key!=enterKey&&key!=upArrowKey&&key!=downArrowKey&&key!=escapeKey)
-            key=getch();
-        if(key==downArrowKey&&option<4)
+        while (key != enterKey && key != upArrowKey && key != downArrowKey && key != escapeKey)
+        {
+            key = getch();
+        }
+        if (key == downArrowKey && option < 4)
+        {
             option++;
-        else if(key==upArrowKey&&option>1)
+        }
+        else if (key == upArrowKey && option > 1)
+        {
             option--;
-    }while(key!=enterKey&&key!=escapeKey);
-    if(key==escapeKey) option=0;
-    if(option==1)
+        }
+    } while (key != enterKey && key != escapeKey);
+    if (key == escapeKey)
+    {
+        option = 0;
+    }
+    if (option == 1)
     {
         player1.resetPlayer();
         initialLoad();
         startGame();
     }
-    else if(option==2)
+    else if (option == 2)
+    {
         printMenu();
-    else if(option==3)
+    }
+    else if (option == 3)
+    {
         helpMenu();
-    else if(option==4)
+    }
+    else if (option == 4)
+    {
         player1.quits();
+    }
 }
 
 int main()
@@ -344,6 +415,6 @@ int main()
     return 0;
 }
 
-///TO DO LIST
-///1.DIFFICULTY LEVEL
-///2.FIX THE OPPOSITE DIRECTION BUG
+// TO DO LIST
+// 1.DIFFICULTY LEVEL
+// 2.FIX THE OPPOSITE DIRECTION BUG
